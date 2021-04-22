c1d4a7a91294299690e8a8d1960ddac73c462ca6
Saddam <visinwork@gmail.com>
2021-04-22 16:38:10 +0300

    'pro-2'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {loadTodos} from "./actions";
+import {useEffect} from "react";
 
 function App() {
+  const todos = useSelector(state => state.todos);
+  const loading = useSelector(state => state.loading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTodos());
+  },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      список дел
+      {loading ? 'идет загрузка' : (
+          todos.map(todo => {
+            return(
+                <div>
+                  {todo.title}
+                </div>
+            )
+          })
+      ) }
     </div>
   );
 }
